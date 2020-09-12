fc2a9eec557ba37542ffd778ac0e78f92a4c5c8e
Shawn Daniels <SGDaniels08@gmail.com>
2020-09-12 13:35:08 -0400

    first commit

---
+++ src/main/java/com/drshawniels/multithreadblog/MultithreadBlogApplication.java
@@ -0,0 +1,13 @@
+package com.drshawniels.multithreadblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadBlogApplication.class, args);
+	}
+
+}
